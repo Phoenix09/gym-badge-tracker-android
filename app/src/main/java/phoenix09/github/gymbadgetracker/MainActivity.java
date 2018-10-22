d1bca193ff7d029dba2544543a5988a90a033ca4
Phoenix09 <Phoenix09@users.noreply.github.com>
2018-10-22 10:26:29 +0100

    Stop redirects getting eaten by the default browser

--- app/src/main/java/phoenix09/github/gymbadgetracker/MainActivity.java
+++ app/src/main/java/phoenix09/github/gymbadgetracker/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
         WebSettings settings = webview.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
